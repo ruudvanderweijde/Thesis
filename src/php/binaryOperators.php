d1b7b7cb92db58d4e180bf76133b8712444e33ba
Ruud van der Weijde <ruudvanderweijde@gmail.com>
2014-08-11 14:49:53 +0200

    added unary and binary operators

---
+++ src/php/binaryOperators.php
@@ -0,0 +1,10 @@
+$a + $b // addition
+$a - $b // subtraction
+$a * $b // mulitiplication
+$a / $b // division
+$a % $b // modulus
+$a & $b // bitwise And
+$a | $b // bitwise Or
+$a ^ $b // bitwise Xor
+$a << $b // bitwise shift left
+$a >> $b // bitwise shift right
---
+++ src/php/unaryOperators.php
@@ -0,0 +1,8 @@
++$a // positive
+-$a // negation
+!$a // not
+~$a // bitwise not
+$a++ // post increase
+$a-- // post decrease
+++$a // pre increase
+--$a // pre decrease
