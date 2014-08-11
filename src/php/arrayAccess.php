4709b5dbeb5dfaab6beaf00f0d3a59dd1013bfd6
Ruud van der Weijde <ruudvanderweijde@gmail.com>
2014-08-11 19:59:00 +0200

    changed Array access prooftree

--- src/php/arrayAccess.php
+++ src/php/arrayAccess.php
@@ -1,4 +1,5 @@
 $a[0];
+// typeOf($a) != object()
 // when typeOf($a) == string() => typeOf($a[/*...*/]) is string()
 // when typeOf($a) == array() => typeOf($a[/*...*/]) is mixed()
 // when typeOf($a) !== string|array => typeOf($a[/*...*/]) is null()
