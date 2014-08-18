9c735a8eefd6770b4ed833b7eaad6d78a7aa7c5d
Ruud van der Weijde <ruudvanderweijde@gmail.com>
2014-08-18 20:41:39 +0200

    mainly update of background information

--- src/php/dynamicProperty.php
+++ src/php/dynamicProperty.php
@@ -1,6 +1,7 @@
 class C {}
-$p = (new C())->nonExistingProperty;
-var_dump($p); // NULL
 $c = new C();
+var_dump($c->nonExistingProperty); // NULL
 $p = $c->nonExistingProperty = "property now exists";
 var_dump($p); // string(19) "property now exists"
+$d = new C;
+var_dump($d->nonExistingProperty); // NULL
