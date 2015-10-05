5ded1fbe52a8abae4e3757d853aecf4ee541ffaf
Ruud van der Weijde <ruudvanderweijde@gmail.com>
2015-10-05 07:39:59 +0200

    text updates

--- src/php/conditionals2.php
+++ src/php/conditionals2.php
@@ -4,7 +4,8 @@ function f() {
   }
 }
 
-g(); f(); // will fail because `g();` is not declared yet
-f(); g(); // will work because `g();` is declared when calling `f();`
-f(); new C(); // will fail because `g();` needs to be called first
-f(); g(); new C(); // will work because `g();` is called and has declared `f();`
+// Execution examples:
+g();                // will fail because `g();' is not declared yet
+f(); g();           // will work because `g();' is declared when calling `f();'
+f(); new C();       // will fail because `g();' needs to be called first
+f(); g(); new C();  // will work because `g();' is called and has declared `f();'
