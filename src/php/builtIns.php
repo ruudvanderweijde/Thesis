2ffc47aa98eb99e402dcb58cbe24fb4aa3441cc9
Ruud van der Weijde <ruudvanderweijde@gmail.com>
2016-08-01 09:19:19 +0200

    lots of additions

---
+++ src/php/builtIns.php
@@ -0,0 +1,23 @@
+/**
+ * Make a string uppercase
+ * @link http://php.net/manual/en/function.strtoupper.php
+ * @param string $string <p>
+ * The input string.
+ * </p>
+ * @return string the uppercased string.
+ * @since 4.0
+ * @since 5.0
+ */
+function strtoupper ($string) {}
+
+/**
+ * Make a string lowercase
+ * @link http://php.net/manual/en/function.strtolower.php
+ * @param string $str <p>
+ * The input string.
+ * </p>
+ * @return string the lowercased string.
+ * @since 4.0
+ * @since 5.0
+ */
+function strtolower ($str) {}
