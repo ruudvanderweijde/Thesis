b74e972047736635d0c9261b5753c9f040e7b9a8
Ruud van der Weijde <ruudvanderweijde@gmail.com>
2017-01-02 01:52:24 +0100

    Review fixes

--- src/php/magicMethods.php
+++ src/php/magicMethods.php
@@ -1,6 +1,6 @@
 class Car {
     private $maxSpeed = 210;
-    function __get($name) { return $this->$name; }
+    function __get($name) { return @$this->$name; }
 }
 var_dump((new Car)->maxSpeed); // 210
 var_dump((new Car)->numberOfWheels); // NULL
